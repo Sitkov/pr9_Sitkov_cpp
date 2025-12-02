86ea9dc1dd13524f6cea264a25afa52b6c36f785
Sveta616 <svetamarinina44@gmail.com>
2025-12-02 17:18:17 +0500

    first

---
+++ father_process/father_process.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    cout << "ProcessArgs is started!" << endl;
+
+    cout << "My name is: ";
+
+    cout << argv[0] << endl;
+
+    for (i = 1; i < argc; ++i) {
+        cout << "Param" << i << " = " << argv[i] << endl;
+    }
+
+    cout << "Press any key to finish this process! \n";
+    _getch();
+    return 0;
+}
